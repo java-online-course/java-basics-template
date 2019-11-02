3620d161a72bc2b8271f093b84e3e4db2e7d7e7e
ilia_isakhin <ilia_isakhin@epam.com>
2019-11-02 19:38:42 +0400

    match game removed

--- src/main/java/com/epam/izh/rd/online/calculator/Main.java
+++
@@ -1,4 +0,0 @@
-package com.epam.izh.rd.online.calculator;
-
-public class Main {
-}
--- src/main/java/com/epam/izh/rd/online/game/Main.java
+++
@@ -1,9 +0,0 @@
-package com.epam.izh.rd.online.game;
-
-public class Main {
-
-    public static void main(String[] args) {
-        new MatchGame(21).playWithFriend();
-    }
-
-}
