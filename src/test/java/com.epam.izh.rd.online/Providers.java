8e59abefa062563faedf768bb8e897492d08455b
Timur_Sokolov <sokolov.tima@gmail.com>
2019-11-12 06:22:06 +0400

    Перенес работу со строками из basic в data-handling

--- src/test/java/com.epam.izh.rd.online/Providers.java
+++ src/test/java/com.epam.izh.rd.online/Providers.java
@@ -73,42 +73,4 @@ public class Providers {
                 arguments(new int[]{4, 4, 5, 5, 2, 6}, new int[]{6, 2, 5, 5, 4, 4})
         );
     }
-
-    public static Stream<Arguments> testRemoveStringProvider() {
-        return Stream.of(
-                arguments("Hello, hello, hello, how low?", ", he", "Hellollollo, how low"),
-                arguments("Ahahahahaha", "ha", "A"),
-                arguments("London is the capital of Great Britain", "Russia", "London is the capital of Great Britain")
-        );
-    }
-
-    public static Stream<Arguments> testIsQuestionStringProvider() {
-        return Stream.of(
-                arguments("Hello, hello, hello, how low?", true),
-                arguments("Hello, hello, hello!", false),
-                arguments("", false)
-        );
-    }
-
-    public static Stream<Arguments> testConcatenateProvider() {
-        return Stream.of(
-                arguments(new String[]{"Smells", " ", "Like", " ", "Teen", " ", "Spirit"}, "Smells Like Teen Spirit"),
-                arguments(new String[]{"", ""}, "")
-        );
-    }
-
-    public static Stream<Arguments> testToJumpCaseProvider() {
-        return Stream.of(
-                arguments("Load Up On Guns And Bring Your Friends", "lOaD Up oN GuNs aNd bRiNg yOuR FrIeNdS"),
-                arguments("", "")
-        );
-    }
-
-    public static Stream<Arguments> testIsPalindromeProvider() {
-        return Stream.of(
-                arguments("а роза упала на лапу Азора", true),
-                arguments("я не палиндром!", false),
-                arguments("", false)
-        );
-    }
 }
