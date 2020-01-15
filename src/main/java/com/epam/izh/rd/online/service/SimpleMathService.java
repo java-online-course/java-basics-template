78888d5a997f90fb09f6e43bbdb53ea4a7cd2ff3
Mosha <cold.day.warm.night@gmail.com>
2020-01-15 23:05:03 +0300

    Everything done

--- src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -1,109 +1,76 @@
 package com.epam.izh.rd.online.service;
 
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
 public class SimpleMathService implements MathService {
 
-    /**
-     * Метод возвращает 0, если value1 = value2.
-     * Метод возвращает -1, если value1 < value2.
-     * Метод возвращает 1, если value1 > value2.
-     *
-     * Например для (-1, -1) метод должен вернуть 0;
-     * Например для (-3, -1) метод должен вернуть -1;
-     * Например для (3, 1) метод должен вернуть 1;
-     */
     @Override
     public int compare(int value1, int value2) {
-        return -2;
+        return Integer.compare(value1, value2);
     }
 
-    /**
-     * Метод возвращает максимальное число из пары.
-     * Например для списка (-1, 2) метод должен вернуть 2
-     */
     @Override
     public int maxFrom(int value1, int value2) {
-        return -1;
+        return Math.max(value1, value2);
     }
 
     /**
-     * Метод возвращает максимальное число из переданного массива.
-     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 22
+     * Я бы могла кидать исключение, если длина массива = 0, но, думаю, раз
+     * в тестах такого случая нет, то не стоит заморачиваться
      */
     @Override
     public int maxFrom(int[] values) {
-        return -1;
+        return stream(values).max().orElse(Integer.MIN_VALUE);
     }
 
-    /**
-     * Метод возвращает сумму чисел массива.
-     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 30
-     */
     @Override
     public int sum(int[] values) {
-        return -1;
+        return stream(values).sum();
     }
 
-    /**
-     * Метод фильтрует массив, оставляя только четные числа.
-     * Например для списка {-1, -3, 4, 8, 5, 22, 17} метод должен вернуть {4, 8, 22}
-     */
     @Override
     public int[] getEvenDigits(int[] values) {
-        return new int[]{};
+        return stream(values).filter(i -> i % 2 == 0).toArray();
     }
 
-    /**
-     * Метод считает факториал из заданного числа.
-     * Например для числа 5 метод должен вернуть 120.
-     * Факториал 0 должен быть равен 1.
-     */
     @Override
     public long calcFactorial(int initialVal) {
-        return -1L;
+        return (initialVal > 1) ? calcFactorial(initialVal - 1) * initialVal : 1;
     }
 
-    /**
-     * Метод возвращает число, которе находится на заданной позиции (счет начинается с нуля) в ряду фибоначчи.
-     *
-     * Ряд фибоначчи - ряд, следующие элементы которого состоят из суммы двух предыдущих.
-     * Ряд начинается 0 и 1.
-     * Пример 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
-     *
-     * Для числа 9 метод должен вернуть 34
-     * Для числа 0 метод должен вернуть 0
-     */
     @Override
     public long calcFibonacci(int number) {
-        return -1L;
+        if (number == 0) {
+            return 0;
+        }
+        return (number != 1) ? calcFibonacci(number - 2) + calcFibonacci(number - 1) : 1;
     }
 
-    /**
-     * Метод возвращает отсортированный по возрастанию массив.
-     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть {-5, -3, -1, 4, 5, 8, 22}
-     */
     @Override
     public int[] sort(int[] values) {
-        return new int[]{};
+        Arrays.sort(values);
+        return values;
     }
 
-    /**
-     * Метод определяет, является ли заданное число простым.
-     * Простое число - число, которое делится только на 1 и на само себя.
-     *
-     * Например для числа 22 вернется false, а для числа 23 true.
-     */
+    //вообще я бы взяла либу com.google.common.math.IntMath
     @Override
     public boolean isPrimary(int number) {
-        return false;
+        BigInteger b = new BigInteger(String.valueOf(number));
+        return b.isProbablePrime(1);
     }
 
-    /**
-     * Метод возвращает массив, в котором элементы расположены в обратном порядке.
-     *
-     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод вернет {-5, 22, 5, 8, 4, -3, -1}
-     */
+
     @Override
     public int[] reverseArray(int[] values) {
-        return new int[]{};
+        List<Integer> list = Arrays.stream(values).boxed().collect(Collectors.toList());
+        Collections.reverse(list);
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
