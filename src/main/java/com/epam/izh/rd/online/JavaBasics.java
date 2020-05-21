74b32cf245a84e9b7ac902541a85f482364447f1
Albert-Fatkhudinov <fasberg2@gmail.com>
2020-05-21 20:35:39 +0300

    Fatkhudinov Albert

--- src/main/java/com/epam/izh/rd/online/JavaBasics.java
+++ src/main/java/com/epam/izh/rd/online/JavaBasics.java
@@ -1,7 +1,12 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.service.SimpleMathService;
+
+import java.util.Scanner;
+
 public class JavaBasics {
     public static void main(String[] args) {
-
+        SimpleMathService simpleMathService = new SimpleMathService();
+        System.out.println(simpleMathService.calcFibonacci(9));
     }
 }
